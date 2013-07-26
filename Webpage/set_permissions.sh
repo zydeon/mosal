7607ebc377fefe672c3e7d1dc37c1f52d190bd40
Pedro Matias <zydeon@gmail.com>
2013-07-26 11:59:26 +0100

    set permissions

---
+++ Webpage/set_permissions.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+chmod -R 755 css/ img/ js/ plugins/
+chmod 644 index.html
+chmod -R 755 align/MOSAL_code/
+chmod -R 755 align/PHPMailer-master/
+chmod 644 align/*.php align/index.html
+chmod 755 align/backend/
+chmod 644 align/backend/.ht*
+chmod 666 align/backend/logs.txt
+chmod -R 777 inputs/
+chmod 755 visual/
+chmod 644 visual/*
