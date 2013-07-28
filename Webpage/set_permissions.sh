ca392076b103c4b58e88ad9291aa886cc3c9a7e1
Pedro Matias <zydeon@gmail.com>
2013-07-28 13:52:47 +0100

    actualizacao de permissoes

--- Webpage/set_permissions.sh
+++ Webpage/set_permissions.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
-chmod -R 755 css/ img/ js/ plugins/
-chmod 644 index.html
-chmod -R 755 align/MOSAL_code/
-chmod -R 755 align/PHPMailer-master/
-chmod 644 align/*.php align/index.php
-chmod 755 align/backend/
-chmod 644 align/backend/.ht*
-chmod 644 align/backend/index.php
-chmod 666 align/backend/logs.html
-chmod -R 777 align/inputs/
-chmod 755 align/visual/
-chmod 644 align/visual/*
-chmod 755 align/subscores
-chmod 644 align/subscores/*
+chmod -R 755 css/ img/ js/ plugins/ includes/
+chmod 644 index.php
+cd align;
+chmod -R 755 MOSAL_code/
+chmod -R 755 PHPMailer-master/
+chmod 644 *.php *.html
+chmod 755 backend/
+chmod 644 backend/.ht*
+chmod 644 backend/*.php
+chmod 666 backend/logs.html
+chmod -R 777 inputs/
+chmod 755 subscores
+chmod 644 subscores/*
