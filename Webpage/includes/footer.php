4b937cb674124fa4dc0d5d6f87a111ff6f5af975
Pedro Matias <zydeon@gmail.com>
2013-11-27 14:30:38 +0100

    updated data

--- Webpage/includes/footer.php
+++ Webpage/includes/footer.php
@@ -4,7 +4,7 @@
 			<hr>
 			<p>Copyright &copy; <?php echo date("Y")?> <a href="http://mosal.dei.uc.pt">MOSAL</a></p>
 			<p>Please <a href="mailto:mosal@dei.uc.pt">contact us <i class="icon-envelope"></i></a> with questions, suggestions or troubles &#8226; This page is optimized for Chrome and Firefox browsers.</p>
-			<p><small>Last update: 21 October 2013</small></p>
+			<p><small>Last update: 27 November 2013</small></p>
 		</div>
 	</center>
 <footer>
