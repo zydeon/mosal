ba0d87968dc0ac0e962985e32ba85a62f21b1c8b
Pedro Matias <zydeon@gmail.com>
2014-01-10 00:16:05 +0100

    paper accepted\!\!\!

--- Webpage/includes/footer.php
+++ Webpage/includes/footer.php
@@ -4,7 +4,7 @@
 			<hr>
 			<p>Copyright &copy; <?php echo date("Y")?> <a href="http://mosal.dei.uc.pt" target="_parent">MOSAL</a></p>
 			<p>Please <a href="mailto:mosal@dei.uc.pt">contact us <i class="icon-envelope"></i></a> with questions, suggestions or troubles &#8226; This page is optimized for Chrome and Firefox browsers.</p>
-			<p><small>Last update: 3 January 2014</small></p>
+			<p><small>Last update: 10 January 2014</small></p>
 		</div>
 	</center>
 <footer>
